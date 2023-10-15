d8d6f9b0464d510f081831c77242ca219f232d6c
Alexandros Kritikos <akritikos@outlook.com>
2023-10-15 23:35:31 +0300

    ✨ Adds a method that returns all parent types

---
+++ src/Extensions.Primitive/TypeExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Kritikos.Extensions.Primitive;
+
+public static class TypeExtensions
+{
+  public static IEnumerable<Type> GetParentTypes(this Type? type)
+  {
+    if (type is null)
+    {
+      yield break;
+    }
+
+    foreach (var i in type.GetInterfaces())
+    {
+      yield return i;
+    }
+
+    var currentBaseType = type.BaseType;
+    while (currentBaseType is not null)
+    {
+      yield return currentBaseType;
+      currentBaseType = currentBaseType.BaseType;
+    }
+  }
+}
